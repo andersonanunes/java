3b76167f49cc22d56e390ee4e2b674b32ca5bb61
andersonanunes <andersonanunes@hotmail.com>
2021-03-09 16:02:34 -0300

    reorganização de pastas

--- FiguraGeometrica/FigGeometrica.java
+++
@@ -1,9 +0,0 @@
-package FiguraGeometrica;
-
-public abstract class FigGeometrica {
-
-    // metodos publicos que vao ser herdados
-    public abstract double calculaPerimetro();
-    public abstract double calculaArea();
-
-}
